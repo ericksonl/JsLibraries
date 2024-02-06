2ae27a3302fe904ee028bdc0e827f0ec3809b74e
Liam <liamerickson20@gmail.com>
2024-02-06 13:43:01 -0800

    Refactor KeyComponents object to an array

--- src/components/DMKeyComponents.js
+++ src/components/DMKeyComponents.js
@@ -8,61 +8,59 @@ import KicknHat from './mp3Files/Kick_n_Hat.mp3';
 import Kick from './mp3Files/Kick.mp3';
 import ClosedHH from './mp3Files/Closed-Hat.mp3';
 
-const KeyComponents = {
-    Heater1: {
-        keyTrigger: 'Q',
-        keyCode: 81,
-        id: 'Heater-1',
-        url: Heater1
-    },
-    Heater2: {
-        keyTrigger: 'W',
-        keyCode: 87,
-        id: 'Heater-2',
-        url: Heater2
-    },
-    Heater3: {
-        keyTrigger: 'E',
-        keyCode: 69,
-        id: 'Heater-3',
-        url: Heater3
-    },
-    Heater4: {
-        keyTrigger: 'A',
-        keyCode: 65,
-        id: 'Heater-4',
-        url: Heater4
-    },
-    Clap: {
-        keyTrigger: 'S',
-        keyCode: 83,
-        id: 'Clap',
-        url: Clap
-    },
-    OpenHH: {
-        keyTrigger: 'D',
-        keyCode: 68,
-        id: 'Open-HH',
-        url: OpenHH
-    },
-    KicknHat: {
-        keyTrigger: 'Z',
-        keyCode: 90,
-        id: 'Kick-n-Hat',
-        url: KicknHat
-    },
-    Kick: {
-        keyTrigger: 'X',
-        keyCode: 88,
-        id: 'Kick',
-        url: Kick
-    },
-    ClosedHH: {
-        keyTrigger: 'C',
-        keyCode: 67,
-        id: 'Closed-HH',
-        url: ClosedHH
-    }
-}
+const KeyComponents = [{
+    keyTrigger: 'Q',
+    keyCode: 81,
+    id: 'Heater-1',
+    url: Heater1
+},
+{
+    keyTrigger: 'W',
+    keyCode: 87,
+    id: 'Heater-2',
+    url: Heater2
+},
+{
+    keyTrigger: 'E',
+    keyCode: 69,
+    id: 'Heater-3',
+    url: Heater3
+},
+{
+    keyTrigger: 'A',
+    keyCode: 65,
+    id: 'Heater-4',
+    url: Heater4
+},
+{
+    keyTrigger: 'S',
+    keyCode: 83,
+    id: 'Clap',
+    url: Clap
+},
+{
+    keyTrigger: 'D',
+    keyCode: 68,
+    id: 'Open-HH',
+    url: OpenHH
+},
+{
+    keyTrigger: 'Z',
+    keyCode: 90,
+    id: 'Kick-n-Hat',
+    url: KicknHat
+},
+{
+    keyTrigger: 'X',
+    keyCode: 88,
+    id: 'Kick',
+    url: Kick
+},
+{
+    keyTrigger: 'C',
+    keyCode: 67,
+    id: 'Closed-HH',
+    url: ClosedHH
+}]
 
 export default KeyComponents;
