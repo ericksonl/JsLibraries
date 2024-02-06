be37d809b7548bb75511c518107eecb0b6adaf3f
Liam <liamerickson20@gmail.com>
2024-02-06 14:09:24 -0800

    Refactor DrumMachine.js to use DMPadBuilder and DMKeyComponents

--- src/pages/DrumMachine.js
+++ src/pages/DrumMachine.js
@@ -1,10 +1,36 @@
-import DMBuilder from "../components/DMBuilder";
+import DMPadBuilder from "../components/DMPadBuilder.js";
+import KeyComponents from '../components/DMKeyComponents.js';
+import React from 'react';
 import '../styles/drumMachine.scss';
+
+class DMBuilder extends React.Component {
+    render() {
+        const KeyProps = this.props.KeyComponents;
+        return (
+            <>
+                <div id='buttons'>
+                    {Object.keys(KeyProps).map((key, index) => (
+                        <DMPadBuilder
+                            key={index}
+                            id={KeyProps[key].id}
+                            keyTrigger={KeyProps[key].keyTrigger}
+                            keyCode={KeyProps[key].keyCode}
+                            url={KeyProps[key].url}
+                        />
+                    ))}
+                </div >
+                <div id='contents'>
+                    <p id='display'>Drums.io</p>
+                </div>
+            </>
+        )
+    }
+}
 
 const DrumMachine = () => {
     return (
         <div id='drum-machine'>
-            <DMBuilder />
+            <DMBuilder KeyComponents={KeyComponents} />
         </div>
     );
 }
