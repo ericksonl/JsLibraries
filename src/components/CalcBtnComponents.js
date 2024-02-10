1d7cdd4ddd88a418cfd95e544b190c5b340fb951
Liam <liamerickson20@gmail.com>
2024-02-10 01:53:22 -0800

    Add CalcBtnComponents to dynamically add calc buttons

---
+++ src/components/CalcBtnComponents.js
@@ -0,0 +1,102 @@
+const CalcBtnComponents = [{
+    id: 'clear',
+    className: 'clear',
+    innerText: 'AC'
+},
+{
+    id: 'delete',
+    className: 'delete',
+    innerText: 'DEL'
+},
+{
+    id: 'percentage',
+    className: 'operator',
+    innerText: '%'
+},
+{
+    id: 'divide',
+    className: 'operator',
+    innerText: '/'
+},
+{
+    id: 'seven',
+    className: 'numbers',
+    innerText: '7'
+},
+{
+    id: 'eight',
+    className: 'numbers',
+    innerText: '8'
+},
+{
+    id: 'nine',
+    className: 'numbers',
+    innerText: '9'
+},
+{
+    id: 'multiply',
+    className: 'operator',
+    innerText: '*'
+},
+{
+    id: 'four',
+    className: 'numbers',
+    innerText: '4'
+},
+{
+    id: 'five',
+    className: 'numbers',
+    innerText: '5'
+},
+{
+    id: 'six',
+    className: 'numbers',
+    innerText: '6'
+},
+{
+    id: 'subtract',
+    className: 'operator',
+    innerText: '-'
+},
+{
+    id: 'one',
+    className: 'numbers',
+    innerText: '1'
+},
+{
+    id: 'two',
+    className: 'numbers',
+    innerText: '2'
+},
+{
+    id: 'three',
+    className: 'numbers',
+    innerText: '3'
+},
+{
+    id: 'add',
+    className: 'operator',
+    innerText: '+'
+},
+{
+    id: 'zero',
+    className: 'numbers',
+    innerText: '0'
+},
+{
+    id: 'double-zero',
+    className: 'numbers',
+    innerText: '00'
+},
+{
+    id: 'decimal',
+    className: 'decimal',
+    innerText: '.'
+},
+{
+    id: 'equals',
+    className: 'equals',
+    innerText: '='
+}];
+
+export default CalcBtnComponents;
