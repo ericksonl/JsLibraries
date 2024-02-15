d08b17c560afc660a8dd0bace63bc4eeeaab6137
Liam <liamerickson20@gmail.com>
2024-02-15 00:45:26 -0800

    Remove moment dependancy

--- src/pages/TwentyFiveFiveClock.js
+++ src/pages/TwentyFiveFiveClock.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
-import moment from 'moment'
 import '../styles/twentyFiveFiveClock.scss'
 
-const defaultSessionLength = 25000 * 60 //25 minutes in milliseconds
+const defaultSessionLength = 25000 * 60; // 60 minutes in milliseconds
 const defaultBreakLength = 5000 * 60 //5 minutes in milliseconds
 
 class TwentyFiveFiveClock extends React.Component {
@@ -11,18 +10,13 @@ class TwentyFiveFiveClock extends React.Component {
         super(props)
         this.state = {
             sessionLength: defaultSessionLength,
-            sessionDisplay: moment(defaultSessionLength).format('m'),
             breakLength: defaultBreakLength,
-            breakDisplay: moment(defaultBreakLength).format('m'),
-            timeLeft: defaultSessionLength,
-            break: false,
+            formatSessionDisplay: 25,
+            formatBreakDisplay: 5,
             hasRun: false,
+            break: false,
             play: false
         }
-        this.changeButton = this.changeButton.bind(this)
-        this.resetTimer = this.resetTimer.bind(this)
-        this.incrementTime = this.incrementTime.bind(this)
-        this.decrementTime = this.decrementTime.bind(this)
     }
 
     componentDidMount() {
@@ -30,10 +24,11 @@ class TwentyFiveFiveClock extends React.Component {
     }
 
     componentWillUnmount() {
-        clearInterval(this.timerInterval); // Clear the interval when component unmounts
+        clearInterval(this.timerInterval);
     }
 
     changeButton = (e) => {
+        console.log(e.target.id)
         this.setState({
             hasRun: true
         })
@@ -41,109 +36,103 @@ class TwentyFiveFiveClock extends React.Component {
             play: !prevState.play // Toggle play state
         }), () => {
             if (this.state.play) {
-                e.target.innerHTML = '<i class="fa fa-pause"></i>'
                 document.getElementById('loading').style.display = 'block'
-
-                document.getElementById("session-increment").disabled = true;
-                document.getElementById("session-decrement").disabled = true;
-                document.getElementById("break-increment").disabled = true;
-                document.getElementById("break-decrement").disabled = true;
-
-
                 this.timerInterval = setInterval(() => {
-                    let formatDate = moment(this.state.sessionLength).subtract(1, 'second')
-                    this.setState(prevState => ({
-                        sessionLength: formatDate
+                    this.state.break ? this.setState(prevState => ({
+                        breakLength: prevState.breakLength - 1000
+                    })) : this.setState(prevState => ({
+                        sessionLength: prevState.sessionLength - 1000
                     }))
                     document.getElementById('loading').style.display = 'none'
+                    if (this.state.breakLength === 0 || this.state.sessionLength === 0) {
+                        clearInterval(this.timerInterval);
+                        this.changeIntervalType()
+                    }
                 }, 1000);
-
-
-                // if (this.state.sessionLength === '00:00') {
-                //     clearInterval(this.timerInterval);
-                //     this.setState({
-                //         sessionLength: this.state.sessionDisplay
-                //     })
-                // }
-
             } else {
-                clearInterval(this.timerInterval); // Clear interval if paused
-                e.target.innerHTML = '<i class="fa fa-play"></i>'
-                document.getElementById("session-increment").disabled = false;
-                document.getElementById("session-decrement").disabled = false;
-                document.getElementById("break-increment").disabled = false;
-                document.getElementById("break-decrement").disabled = false;
+                clearInterval(this.timerInterval);
             }
         });
     }
 
-    //TODO: Handle 60 minutes converting to hour
-    decrementTime = (e) => {
+    changeTime = (e) => {
+        console.log(e.target.id)
         let id = e.target.id
         let timeType = id.split('-')[0]
-        // TODO: If timer has been running, and user clicks on the increment or decrement button, the timer should reset to the new session length
+        let timeAction = id.split('-')[1]
+        if (this.state.hasRun) {
+            clearInterval(this.timerInterval)
+            this.setState({
+                sessionLength: this.state.formatSessionDisplay * 60 * 1000,
+                breakLength: this.state.formatBreakDisplay * 60 * 1000,
+            })
+        }
 
-        if (timeType === 'break' && this.state.breakDisplay > 1) {
-            this.setState({
-                breakLength: moment(this.state.breakLength).subtract(1, 'minutes'),
-                breakDisplay: moment(this.state.breakLength).subtract(1, 'minutes').format('m')
-            })
-        } else if (timeType === 'session' && this.state.sessionDisplay > 1) {
-            this.setState({
-                sessionLength: moment(this.state.sessionLength).subtract(1, 'minutes'),
-                sessionDisplay: moment(this.state.sessionLength).subtract(1, 'minutes').format('m')
-            })
-        } else {
-            return
+        if (timeType === 'break') {
+            if (timeAction === 'increment' && this.state.breakLength < 60 * 1000 * 60) {
+                this.setState(prevState => ({
+                    breakLength: prevState.breakLength + 1000 * 60,
+                    formatBreakDisplay: prevState.formatBreakDisplay + 1
+                }))
+                return
+            } else if (timeAction === 'decrement' && this.state.breakLength > 1000 * 60) {
+                this.setState(prevState => ({
+                    breakLength: prevState.breakLength - 1000 * 60,
+                    formatBreakDisplay: prevState.formatBreakDisplay - 1
+                }))
+                return
+            }
+        } else if (timeType === 'session') {
+            if (timeAction === 'increment' && this.state.sessionLength < 60 * 1000 * 60) {
+                this.setState(prevState => ({
+                    sessionLength: prevState.sessionLength + 1000 * 60,
+                    formatSessionDisplay: prevState.formatSessionDisplay + 1
+                }))
+                return
+            } else if (timeAction === 'decrement' && this.state.sessionLength > 1000 * 60) {
+                this.setState(prevState => ({
+                    sessionLength: prevState.sessionLength - 1000 * 60,
+                    formatSessionDisplay: prevState.formatSessionDisplay - 1
+                }))
+                return
+            }
         }
     }
 
-    incrementTime = (e) => {
-        let id = e.target.id
-        let timeType = id.split('-')[0]
-
-        // TODO: If timer has been running, and user clicks on the increment or decrement button, the timer should reset to the new session length
-        if (timeType === 'break' && this.state.breakDisplay < 60) {
-            this.setState({
-                breakLength: moment(this.state.breakLength).add(1, 'minutes'),
-                breakDisplay: moment(this.state.breakLength).add(1, 'minutes').format('m')
-            })
-        } else if (timeType === 'session' && this.state.sessionDisplay < 60) {
-            this.setState({
-                sessionLength: moment(this.state.sessionLength).add(1, 'minutes'),
-                sessionDisplay: moment(this.state.sessionLength).add(1, 'minutes').format('m')
-            })
-        } else {
-            return
-        }
+    changeIntervalType = () => {
+        clearInterval(this.timerInterval);
+        this.setState(prevState => ({
+            sessionLength: prevState.formatSessionDisplay * 60 * 1000,
+            breakLength: prevState.formatBreakDisplay * 60 * 1000,
+            break: !prevState.break,
+            play: false
+        }), () => {
+            document.getElementById('timer-label').innerHTML = this.state.break ? 'Break' : 'Session';
+        });
     }
 
     resetTimer = () => {
-        clearInterval(this.timerInterval); // Clear the interval when component unmounts
+
+        clearInterval(this.timerInterval);
+        document.getElementById('timer-label').innerHTML = 'Session'
         this.setState({
             sessionLength: defaultSessionLength,
-            sessionDisplay: moment(defaultSessionLength).format('m'),
             breakLength: defaultBreakLength,
-            breakDisplay: moment(defaultBreakLength).format('m'),
-            timeLeft: defaultSessionLength,
-            break: false,
+            formatSessionDisplay: 25,
+            formatBreakDisplay: 5,
             hasRun: false,
+            break: false,
             play: false
         })
-        document.getElementById('start_stop').innerHTML = '<i class="fa fa-play"></i>'
-        document.getElementById("session-increment").disabled = false;
-        document.getElementById("session-decrement").disabled = false;
-        document.getElementById("break-increment").disabled = false;
-        document.getElementById("break-decrement").disabled = false;
     }
 
     render() {
         //TODO: Format time to display in mm:ss AT 60 minutes
-        let formatSession = moment(this.state.sessionLength).format('mm:ss')
-        let formatBreak = moment(this.state.breakLength).format('mm:ss')
-        let sessionDisplay = this.state.sessionDisplay
-        let breakDisplay = this.state.breakDisplay
+        let minutes = Math.floor((this.state.break ? this.state.breakLength : this.state.sessionLength) / 1000 / 60)
+        let seconds = Math.floor((this.state.break ? this.state.breakLength : this.state.sessionLength) / 1000 % 60)
+        seconds = seconds < 10 ? '0' + seconds : seconds
 
+        let timeLeft = minutes + ':' + seconds
         return (
             <div id='clock-wrapper'>
                 <h1>25+5 Clock</h1>
@@ -151,23 +140,26 @@ class TwentyFiveFiveClock extends React.Component {
                     <h2 id="timer-label">Session</h2>
                     <h3 id="loading">loading...</h3>
                     <div id='clock-display'>
-                        <Button id="start_stop" variant='clear' onClick={this.changeButton}><i className="fa fa-play"></i></Button>
-                        <div id="time-left">{this.state.break ? formatBreak : formatSession}</div>
+                        <Button id="start_stop" variant='clear' onClick={this.changeButton}>{this.state.play ? <i className="fa fa-pause"></i> : <i className="fa fa-play"></i>}</Button>
+                        <div id="time-left">
+                            {timeLeft}
+                            {/* <audio id="beep" src="https://www.soundjay.com/button/beep-07.wav" type="audio/wav"></audio> */}
+                        </div>
                         <Button id="reset" variant='clear' onClick={this.resetTimer}><i className="fa fa-refresh"></i></Button>
                     </div>
                 </div>
                 <div id='controls'>
                     <div id='time-controls'>
                         <div id="break-label">Break Length</div>
-                        <Button id="break-increment" variant='clear' onClick={this.incrementTime}><i className="fa fa-arrow-up"></i></Button>
-                        <h3 id="break-length">{breakDisplay}</h3>
-                        <Button id="break-decrement" variant='clear' onClick={this.decrementTime}><i className="fa fa-arrow-down"></i></Button>
+                        <Button id="break-increment" variant='clear' onClick={this.changeTime} disabled={this.state.play ? true : false}><i className="fa fa-arrow-up"></i></Button>
+                        <h3 id="break-length">{this.state.formatBreakDisplay}</h3>
+                        <Button id="break-decrement" variant='clear' onClick={this.changeTime} disabled={this.state.play ? true : false}><i className="fa fa-arrow-down"></i></Button>
                     </div>
                     <div id='time-controls'>
                         <div id="session-label">Session Length</div>
-                        <Button id="session-increment" variant='clear' onClick={this.incrementTime}><i className="fa fa-arrow-up"></i></Button>
-                        <h3 id="session-length">{sessionDisplay}</h3>
-                        <Button id="session-decrement" variant='clear' onClick={this.decrementTime}><i className="fa fa-arrow-down"></i></Button>
+                        <Button id="session-increment" variant='clear' onClick={this.changeTime} disabled={this.state.play ? true : false}><i className="fa fa-arrow-up"></i></Button>
+                        <h3 id="session-length">{this.state.formatSessionDisplay}</h3>
+                        <Button id="session-decrement" variant='clear' onClick={this.changeTime} disabled={this.state.play ? true : false}><i className="fa fa-arrow-down"></i></Button>
                     </div>
                 </div>
             </div>
